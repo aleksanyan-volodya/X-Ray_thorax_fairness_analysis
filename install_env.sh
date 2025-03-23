6a9a32a8569693679b8796c911160f0d18a6692f
Volodya Aleksanyan <volodya.aleksanyan13@gmail.com>
2025-03-23 12:12:50 +0100

    .gitignore and install_env.sh are added.
    env files be ignored and to install a new env .install_env.sh will work for every OS

--- install_env.sh
+++ install_env.sh
@@ -1,4 +1,9 @@
-python3.10 -m venv projet-env
-source projet-env/bin/activate
+#!/bin/bash
+python3 -m venv projet-env
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    source projet-env/Scripts/activate  # Windows
+else
+    source projet-env/bin/activate  # Linux/macOS
+fi
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
