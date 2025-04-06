209f8753a654fe2aa1d564d29723c01bd198f8b5
Volodya Aleksanyan <volodya.aleksanyan13@gmail.com>
2025-04-06 16:18:25 +0200

    funtions to class

--- image_transformation.py
+++ image_transformation.py
@@ -196,27 +196,130 @@ def remove_transformed_data(csv_path: str) -> int:
     
     return len(df[df['Image Index'].str.contains('_transformed.png')])
 
-"""
-### try class
-### including all functions above
-class ImageTransformation:
-    def __init__(self, image_path: str, rotation: int = 0, brightness: float = 1.0, noise: float = 0.0, blur: float = 0.0, equalize: bool = False):
-        self.image_path = image_path
-        self.rotation = rotation
-        self.brightness = brightness
-        self.noise = noise
-        self.blur = blur
-        self.equalize = equalize
-        self.image = Image.open(image_path)
-     
-    #    def create_transformed_image(image_path: str, transformed_image_path: str, rotation: int = 0, brightness: float = 1.0, noise: float = 0.0, blur: float = 0.0, equalize: bool = False) -> None
-#
- #       def add_transformed_image_to_csv(from_csv_path: str, image_index: str, transformed_image_index: str, to_csv_path: str = None) -> None
-#
- #       def add_new_image(from_image_path: str, from_csv_path: str, to_csv_path: str = None, to_image_path: str = None, rotation: int = 0, brightness: float = 1.0, noise: float = 0.0, blur: float = 0.0, equalize: bool = False) -> None
-#
- #       def remove_image(image_path: str) -> None
-#
- #       def remove_transformed_data(csv_path: str) -> int
-  #  
-"""
+
+# ### try class
+# ### including all functions above
+# class ImageTransformation:
+#     def __init__(self, image_path: str, rotation: int = 0, brightness: float = 1.0, noise: float = 0.0, blur: float = 0.0, equalize: bool = False):
+#         self.image_path = image_path
+#         self.image = Image.open(image_path)
+
+#         self.rotation = rotation
+#         self.brightness = brightness
+#         self.noise = noise
+#         self.blur = blur
+#         self.equalize = equalize
+
+#         self.transformed_image = None
+#         self.transformed_image_path = None
+
+
+#     def __rotate_image(self) -> None:
+#         """
+#         Rotate an image by a given angle
+#         """
+#         self.transformed_image = self.image.rotate(self.angle)
+#         return
+    
+#     def __change_brightness(self) -> None:
+#         """
+#         Change the brightness of an image
+#         """
+#         enhancer = ImageEnhance.Brightness(self.image)
+#         self.transformed_image = enhancer.enhance(self.brightness)
+#         return
+    
+#     def __add_noise(self) -> None:
+#         """
+#         Add noise to an image
+#         """
+#         np_image = np.array(self.image)
+#         noise = np.random.normal(0, self.noise, np_image.shape)
+#         noisy_image = np_image + noise
+#         noisy_image_clipped = np.clip(noisy_image, 0, 255)
+#         self.transformed_image = Image.fromarray(noisy_image_clipped.astype(np.uint8))
+#         return
+    
+#     def __add_blur(self) -> None:
+#         """
+#         Add blur to an image
+#         """
+#         self.transformed_image = self.image.filter(ImageFilter.GaussianBlur(self.blur))
+#         return
+    
+#     def __equalization(self) -> None:
+#         """
+#         Equalize the histogram of an image
+#         """
+#         eq = ImageEnhance.Contrast(self.image).enhance(2)
+#         self.transformed_image = eq
+#         return
+
+#     def transform_image(self) -> None:
+#         """
+#         Transform an image by applying rotation, brightness, saturation, noise and blur
+#         """ 
+
+#         if self.rotation != 0:
+#             self.__rotate_image()
+#         if self.brightness != 1.0:
+#             self.__change_brightness()
+#         if self.noise != 0.0:
+#             self.__add_noise()
+#         if self.blur != 0.0:
+#             self.__add_blur()
+#         if self.equalize:
+#             self.__equalization()
+        
+#         return
+
+#     def save_transformed_image(self, transformed_image_path: str) -> None:
+#         """
+#         Save the transformed image to a file
+#         :param transformed_image_path: str
+#         """
+#         self.transform_image()
+#         if self.transformed_image is not None:
+#             self.transformed_image.save(transformed_image_path)
+#             self.transformed_image_path = transformed_image_path
+#         else:
+#             raise ValueError("Transformed image is None. Please transform the image before saving.")
+
+#     def show_transformed_image(self) -> None:
+#         """
+#         Show the transformed image
+#         """
+#         if self.transformed_image is None:
+#             self.transformed_image = self.image.copy()
+        
+#         plt.imshow(self.transformed_image)
+#         plt.axis('off')
+#         plt.show()
+    
+#     def add_transformed_image_csv(self, csv_path: str, image_index: str, transformed_image_index: str) -> None:
+#         """
+#         Add the transformed image to the csv file
+#         :param csv_path: str
+#         :param image_index: str
+#         :param transformed_image_index: str
+#         """
+#         df = pd.read_csv(csv_path)
+#         image_info = df[df['Image Index'] == image_index].copy()
+#         image_info['Image Index'] = transformed_image_index
+        
+#         # add the new image to the end of the csv
+#         df = pd.concat([df, image_info], ignore_index=True)
+        
+#         df.to_csv(csv_path, index=False)
+
+# """
+# def add_transformed_image_to_csv(from_csv_path: str, image_index: str, transformed_image_index: str, to_csv_path: str) -> None:
+
+# def add_new_image(from_image_path: str, from_csv_path: str, to_csv_path: str = None, to_image_path: str = None, rotation: int = 0, brightness: float = 1.0, noise: float = 0.0, blur: float = 0.0, equalize: bool = False) -> None:
+
+# def remove_image(image_path: str) -> None:
+
+
+# def remove_transformed_data(csv_path: str) -> int:
+
+# """
